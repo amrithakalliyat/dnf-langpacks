96d00ae2b246ad03015473494b2adb0da275d048
amrithakalliyat <amrithakalliyat@gmail.com>
2015-01-30 15:05:01 +0530

    test

---
+++ dnf.sh
@@ -0,0 +1,8 @@
+yum -y update
+rpm -Uhv dnf-langpacks-0.6.0-1.fc21.noarch.rpm
+echo "test the command 'dnf langavailable'"
+rpm -q dnf-langpacks |grep "package is installed"
+dnf langavailable hindi |grep "hindi is available"
+echo "test pass"
+
+
